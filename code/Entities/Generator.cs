965b9e641f86a0271cbe0844400572be07b2f396
Ian <skalenowsky@gmail.com>
2022-12-11 17:33:09 +0300

    generator entity

---
+++ code/Entities/Generator.cs
@@ -0,0 +1,17 @@
+﻿using Editor;
+using Sandbox;
+
+namespace Frightworks;
+
+[Library( "frightworks_generator" ), HammerEntity]
+[Title( "Generator" )]
+[EditorModel( "models/generator/generator.vmdl" )]
+public partial class Generator : ModelEntity
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/generator/generator.vmdl" );
+	}
+}
