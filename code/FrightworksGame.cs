0cb9980e2699e1c0f8be42fc72e9cc960cdb772a
Ian <skalenowsky@gmail.com>
2022-12-11 14:59:32 +0300

    Add project files.

---
+++ code/FrightworksGame.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox;
+
+namespace Frightworks;
+
+public partial class FrightworksGame : GameManager
+{
+	public override void ClientJoined( IClient cl )
+	{
+		base.ClientJoined( cl );
+
+		var plr = new Player();
+		cl.Pawn = plr;
+	}
+}
---
+++ code/Player.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox;
+
+namespace Frightworks;
+
+public partial class Player : AnimatedEntity
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Tags.Add( "player" );
+		SetModel( "models/citizen/citizen.vmdl" );
+	}
+}
