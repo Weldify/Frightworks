fd8068fb5f46c6101b8992ade3f7452b2f6e61ad
Ian <skalenowsky@gmail.com>
2022-12-11 20:55:35 +0300

    gas can

---
+++ code/Entities/GasCan.cs
@@ -0,0 +1,12 @@
+﻿using Editor;
+using Sandbox;
+
+namespace Frightworks;
+
+[Library( "frightworks_gas_can" ), HammerEntity]
+[Title( "Gas Can" )]
+[EditorModel( "models/gas_can/gas_can.vmdl" )]
+public partial class GasCan : ModelEntity
+{
+
+}
