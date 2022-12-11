01f83b696951792a359592cc6b14abbf10249aa2
Ian <skalenowsky@gmail.com>
2022-12-11 15:33:18 +0300

    player view angles

--- code/Player.cs
+++ code/Player.cs
@@ -4,11 +4,25 @@ namespace Frightworks;
 
 public partial class Player : AnimatedEntity
 {
+	[ClientInput]
+	public Angles ViewAngles { get; set; }
+
 	public override void Spawn()
 	{
 		base.Spawn();
 
 		Tags.Add( "player" );
 		SetModel( "models/citizen/citizen.vmdl" );
+	}
+
+	public override void BuildInput()
+	{
+		var view = ViewAngles;
+		view += Input.AnalogLook;
+		view.pitch = view.pitch.Clamp( -89, 89f );
+		view.roll = 0f;
+		ViewAngles = view.Normal;
+
+		base.BuildInput();
 	}
 }
