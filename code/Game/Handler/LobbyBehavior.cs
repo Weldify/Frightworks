ff76c8ffb5f7c6cca5741fb9b0ab2aec25db7e05
Ian <skalenowsky@gmail.com>
2022-12-12 23:22:12 +0300

    lobby countdown

--- code/Game/Handler/LobbyBehavior.cs
+++ code/Game/Handler/LobbyBehavior.cs
@@ -1,16 +1,39 @@
 ﻿using Sandbox;
+using System.Linq;
 
 namespace Frightworks;
 
 public partial class LobbyBehavior : GameBehavior
 {
-	public override void Activate()
-	{
-		Log.Info( "Heyea" );
-	}
+	[Net]
+	public TimeUntil TimeUntilStart { get; set; } = GameSettings.LobbyCountdownTime;
 
 	public override BasePlayer CreatePlayerForClient( IClient cl )
 	{
 		return new LobbyPlayer();
+	}
+
+	bool IsEveryoneReady()
+	{
+		var allPlayers = Entity.All.OfType<LobbyPlayer>();
+		var plrCount = allPlayers.Count();
+
+		if ( plrCount == 0 )
+			return false;
+		if ( !GameSettings.IsDebug && plrCount < 2 )
+			return false;
+
+		var readyPlayers = allPlayers.Where( p => p.ReadyAs != ReadyAs.None );
+
+		return allPlayers.Count() == readyPlayers.Count();
+	}
+
+	public override void Update()
+	{
+		if ( !IsEveryoneReady() )
+		{
+			TimeUntilStart = GameSettings.LobbyCountdownTime;
+			return;
+		}
 	}
 }
---
+++ code/GameSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Frightworks;
+
+public static class GameSettings
+{
+	public static bool IsDebug => true;
+
+	public static float LobbyCountdownTime => 10f;
+}
