035219907326f118638caa5150502ac01923809a
Ian <skalenowsky@gmail.com>
2022-12-14 01:15:37 +0300

    folder refactor

---
+++ code/Game/Behavior/Match/MatchBehavior.State.cs
@@ -0,0 +1,105 @@
+﻿using Sandbox;
+using System.Linq;
+
+namespace Frightworks;
+
+public enum MatchState
+{
+	WaitingForPlayers,
+	Ongoing,
+	Ended,
+}
+
+public partial class MatchBehavior
+{
+	[Net]
+	public MatchState State { get; set; } = MatchState.WaitingForPlayers;
+
+	bool CanStartRound()
+	{
+		foreach ( (var steamId, var _) in PlayerRoles )
+		{
+			var exists = Game.Clients.Any( c => c.SteamId == steamId );
+			if ( !exists ) return false;
+		}
+
+		return true;
+	}
+
+	void SpawnPlayers()
+	{
+		var survivorSpawns = Entity.All.OfType<SurvivorSpawn>();
+		var slasherSpawns = Entity.All.OfType<SlasherSpawn>();
+
+		var survivorSpawnId = 0;
+		var slasherSpawnId = 0;
+
+		foreach ( var (steamId, readyAs) in PlayerRoles )
+		{
+			var client = Game.Clients.First( c => c.SteamId == steamId );
+
+			BasePlayer plr = readyAs switch
+			{
+				ReadyAs.Slasher => new SlasherPlayer(),
+				ReadyAs.Survivor or _ => new SurvivorPlayer(),
+			};
+			client.Pawn = plr;
+
+			if ( plr is SurvivorPlayer surv )
+			{
+				var spawn = survivorSpawns.ElementAt( survivorSpawnId );
+				surv.Transform = spawn.Transform;
+				surv.ResetInterpolation();
+
+				survivorSpawnId = (survivorSpawnId + 1) % survivorSpawns.Count();
+			}
+
+			if ( plr is SlasherPlayer slasher )
+			{
+				var spawn = slasherSpawns.ElementAt( slasherSpawnId );
+				slasher.Transform = spawn.Transform;
+				slasher.ResetInterpolation();
+
+				slasherSpawnId = (slasherSpawnId + 1) % slasherSpawns.Count();
+			}
+		}
+	}
+
+	void SpawnObjectives()
+	{
+		// Generators
+		foreach ( var genSpawn in Entity.All.OfType<GeneratorSpawn>() )
+		{
+
+		}
+	}
+
+	void MakeStateOngoing()
+	{
+		State = MatchState.Ongoing;
+
+		SpawnPlayers();
+		SpawnObjectives();
+	}
+
+	public void UpdateState()
+	{
+		// Weird ass state machine
+		while ( true )
+		{
+			switch ( State )
+			{
+				case MatchState.WaitingForPlayers:
+					if ( CanStartRound() )
+					{
+						MakeStateOngoing();
+						continue;
+					}
+
+					break;
+			}
+
+			break;
+		}
+	}
+}
