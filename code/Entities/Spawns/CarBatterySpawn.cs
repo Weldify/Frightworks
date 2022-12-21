6e4d3d58a754c5fa8a61a126730ebd5cdbf67287
Ian <skalenowsky@gmail.com>
2022-12-21 04:53:51 +0300

    something

--- code/Entities/Spawns/CarBatterySpawn.cs
+++ code/Entities/Spawns/CarBatterySpawn.cs
@@ -6,7 +6,7 @@ namespace Frightworks;
 [Library( "fw_car_battery" ), HammerEntity]
 [Title( "Car Battery Spawn" ), Category( "Objective" )]
 [EditorModel( "models/car_battery/car_battery.vmdl" )]
-public partial class CarBatterySpawn : Entity
+public partial class CarBatterySpawn : Entity 
 {
 	[Property( Title = "Generator Id" )]
 	public int GeneratorId { get; set; } = 0;
